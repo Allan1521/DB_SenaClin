cb1f1127d2de9a0b7e72930a14f8d89034210215
Allan <>
2024-06-05 19:39:26 -0300

    exercicio7

---
+++ exercicio7.sql
@@ -0,0 +1,5 @@
+/*exercicio7*/
+
+UPDATE consulta SET tipoConsulta = 'Tratamento será realizado em 10 consultas. 
+Prioridade: Moderada. Remédio aplicado: Postan, caso sinta dores', 
+consulta.tipoConsulta = 'Tratamento' WHERE idPaciente = 1;
