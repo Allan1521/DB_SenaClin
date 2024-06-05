deb28178496c82ed3d6b6660522560b91efd0a59
Allan <>
2024-06-05 20:03:41 -0300

    exercicio.3

---
+++ exercicio.3.sql
@@ -0,0 +1,12 @@
+/*exercicio3*/
+
+INSERT INTO dentista(
+nomeDentista,CRO,especialidade,Tel_Dentista,Cel_Dentista) 
+VALUES 
+	('Paula Romeu','202400SP','Periodontia','34692200','991112222'),
+	('Isaac Romeu Oliveir','305100SP','Ortodontia','34692200','991335568'),
+	('José Carlos Brandão','504500SP','Implantodontia','34692200','988556677'),
+	('Júlia Garcia','115689SP','Geral','34692200','992525300');	
+	
+	
+SELECT*FROM dentista
