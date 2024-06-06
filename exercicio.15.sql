28d274aa6bc9ecf358a9aae110c492be91a24c79
Allan <>
2024-06-05 22:24:40 -0300

    exercicio.15

--- exercicio.15.sql
+++ exercicio.15.sql
@@ -1,5 +1,12 @@
+
 /*exercicio.15  15.Criar uma query que traga todas as consultas da especialidade implantodontia. 
 Deve vir na query o nome do dentista, o cro, a data da consulta e o nome do paciente, 
 ordenados da data mais atual para a mais antiga.*/
 
-UPDATE dentista SET especialidade='Implantodontia' WHERE dentista.idDentista = 3;
+SELECT dentista.nomeDentista, dentista.cro, consulta.Data_Consulta, paciente.nome  FROM consulta
+INNER JOIN dentista 
+ON consulta.idDentista = dentista.idDentista 
+INNER JOIN paciente 
+ON paciente.idPaciente = consulta.idPaciente
+WHERE dentista.especialidade = 'Implantodontia'
+ORDER BY consulta.Data_Consulta DESC;
