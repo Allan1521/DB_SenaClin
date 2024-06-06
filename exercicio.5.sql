c283474a2cdedc83fd0ce8c415a4bad78e9a026a
Allan <>
2024-06-05 21:09:36 -0300

    exercicio.5

--- exercicio.5.sql
+++ exercicio.5.sql
@@ -1,4 +1,5 @@
-/*exercicio5*/
+/*exercicio5 5.Atualizar todos os dados, exceto nome e cro, do dentista que cuida de implantodontia, 
+mudando sua especialidade para Geral;*/
 
 UPDATE dentista SET Tel_Dentista = '34679000', Cel_Dentista = '991234567', especialidade = 'Geral' WHERE idDentista = 3; 
 
