d7b22a740922ed4e15ec8f0db1286092af4517c4
Allan <>
2024-06-05 21:10:32 -0300

    exercicio.7

--- exercicio.7.sql
+++ exercicio.7.sql
@@ -1,4 +1,7 @@
-/*exercicio7*/
+/*exercicio 7.	Atualizar a consulta do dentista de especialidade ortodontia, mudando o tipo de consulta para 
+Tratamento e inserindo uma observação do dentista com o seguinte texto: “Tratamento será realizado em 10 consultas. 
+Prioridade: Moderada. Remédio aplicado: Ponstan, caso sinta dores”
+*/
 
 UPDATE consulta SET tipoConsulta = 'Tratamento será realizado em 10 consultas. 
 Prioridade: Moderada. Remédio aplicado: Postan, caso sinta dores', 
