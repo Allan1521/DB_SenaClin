7d136fd66ac93dad439e243c72d0e02630d14482
Allan <>
2024-06-05 21:10:04 -0300

    exercicio.6

--- exercicio.6.sql
+++ exercicio.6.sql
@@ -1,3 +1,3 @@
-/*exercicio6*/
+/*exercicio6 6.Atualizar a data e hora de uma consulta marcada com um dentista cuja especialidade é Geral;*/
 
 UPDATE consulta SET Data_Consulta = '2024-07-15', Hora_Consulta = '11:15:03' WHERE idPaciente = 6;
