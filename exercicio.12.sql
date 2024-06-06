4158ff4baa733bea2052204bd139aee3793b9236
Allan <>
2024-06-05 22:03:11 -0300

    exercicio.12

--- exercicio.12.sql
+++ exercicio.12.sql
@@ -2,4 +2,4 @@
  período do ano. Escolher um mês que retorne ao menos uma consulta  e incluir o mês como filtro. */
  
  SELECT COUNT(consulta.idConsulta) AS 'Consultas Julho/2024' FROM consulta
- WHERE consulta.Data_Consulta >= '2024-07-01' AND consulta.Data_Consulta < '2024-07-31';
+ WHERE consulta.Data_Consulta >= '2024-07-01' AND consulta.Data_Consulta <= '2024-07-31';
